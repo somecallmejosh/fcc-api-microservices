245c37232ba43401af6e1daa8662cbec66a487aa
Joshua Briley <josh@thebrileys.com>
2021-06-06 19:11:47 -0400

    Initial Commit.

---
+++ server.js
@@ -0,0 +1,51 @@
+'use strict'
+
+var fs = require('fs')
+var express = require('express')
+var app = express()
+
+if(!process.env.DISABLE_XORIGIN) {
+  app.use(function(req, res, next){
+    var alloweOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com']
+    var origin = req.headers.origin || '*'
+    if(!process.env.XORIGIN_RESTRICT || allowedOrigins.indexOf(origin) > -1) {
+      console.log(origin)
+      res.setHeader('Access-Control-Allow-Origin', origin)
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    }
+    next()
+  })
+}
+
+app.use('/public', express.static(process.cwd() + '/public'))
+
+app.route('/_api/package.json')
+  .get(function(req, res, next){
+    console.log('requestes')
+    fs.readFile(__dirname + '/package.json', function(err, data) {
+      if(err) return next(err)
+      res.type('txt').send(data.toString())
+    })
+  })
+
+app.route('/')
+  .get(function(req,res) {
+    res.sendFile(process.cwd() + '/views/index.html')
+  })
+
+app.use(function(req, res, next) {
+  res.status(404)
+  res.type('txt').send('Not found')
+})
+
+app.use(function(err, req, res, next) {
+  if(err) {
+    res.status(err.status || 500)
+      .type('txt')
+      .send(err.message || 'SERVER ERROR')
+  }
+})
+
+app.listen(process.env.PORT, function() {
+  console.log('Node.js listening...')
+})
